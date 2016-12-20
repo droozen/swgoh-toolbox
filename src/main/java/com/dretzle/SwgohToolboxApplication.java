e7fa94570e61c1076fefd2c447fb28009e358e2c
droozen <droozenrule@gmail.com>
2016-12-20 14:33:39 -0500

    Initial project generation

---
+++ src/main/java/com/dretzle/SwgohToolboxApplication.java
@@ -0,0 +1,12 @@
+package com.dretzle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwgohToolboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwgohToolboxApplication.class, args);
+	}
+}
---
+++ src/test/java/com/dretzle/SwgohToolboxApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dretzle;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SwgohToolboxApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
